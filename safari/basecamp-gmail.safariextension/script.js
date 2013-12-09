1f40368dc95384adfdd099865c58738e1f68e827
Travis Jeffery <tj@travisjeffery.com>
2013-12-08 20:41:35 -0500

    Add link when open email directly rather than just mail from your inboox

--- safari/basecamp-gmail.safariextension/script.js
+++ safari/basecamp-gmail.safariextension/script.js
@@ -1,23 +1,28 @@
-window.addEventListener('hashchange', function(){
-  var link = document.querySelector('.basecamp-link')
-  var title =  document.querySelector('span.hP')
-  if (title && link && title.textContent.match(/Daily Recap/)) link.style.display = 'none'
-  else {
-    var links = document.querySelectorAll('[href^="https://basecamp.com/"]')
-    if (!links.length) return;
-    var href = links[0].href
-    if (!link) {
-      link = document.createElement('a')
-      link.className = 'basecamp-link T-I J-J5-Ji lS T-I-ax7 ar7'
-      link.target = '_blank'
++function() {
+  var createOrUpdateLink = function () {
+    var link = document.querySelector('.basecamp-link')
+    var title =  document.querySelector('span.hP')
+    if (title && link && title.textContent.match(/Daily Recap/)) link.style.display = 'none'
+    else {
+      var links = document.querySelectorAll('[href^="https://basecamp.com/"]')
+      if (!links.length) return;
+      var href = links[0].href
+      if (!link) {
+        link = document.createElement('a')
+        link.className = 'basecamp-link T-I J-J5-Ji lS T-I-ax7 ar7'
+        link.target = '_blank'
+        link.href = href
+        link.appendChild(document.createTextNode('Visit this on Basecamp'))
+        document.querySelector('.iH > div').appendChild(link)
+      }
+      link.style.display = ''
       link.href = href
-      link.appendChild(document.createTextNode('Visit this on Basecamp'))
-      document.querySelector('.iH > div').appendChild(link)
     }
-    link.style.display = ''
-    link.href = href
   }
-})
+
+  window.addEventListener('hashchange', createOrUpdateLink)  
+  window.addEventListener('DOMFocusIn', createOrUpdateLink)  
+}()
 document.addEventListener('keypress', function(event){
   if (event.target.className.match(/editable/)) return;
   var link = document.querySelector('.basecamp-link')
